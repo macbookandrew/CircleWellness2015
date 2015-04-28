d564e96e399de950495a9967a01f92ea6ac4d7a1
Andrew Minion <andrew@andrewrminion.com>
2015-04-28 12:59:46 -0400

    add conditional check for ACF before calling the function

--- functions.php
+++ functions.php
@@ -23,8 +23,10 @@ add_image_size( 'home_quick_link', '228', '137' );
 // check for existence of backgorund image and add to post_class
 function add_custom_background_class( $classes ) {
     global $post;
-    if ( get_field( 'background_image', $post->ID ) ) {
-        $classes[] = 'custom-background';
+    if ( defined( get_field ) ) { // conditional check for ACF installation
+        if ( get_field( 'background_image', $post->ID ) ) {
+            $classes[] = 'custom-background';
+        }
     }
     return $classes;
 }
