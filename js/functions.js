a57c876cb87b29cdfccac2c449ca8433c891c9fb
Andrew Minion <andrew@andrewrminion.com>
2016-05-09 16:48:11 -0400

    remove hacky social quick-link button JS and styles

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function(){
-    // handle Facebook and Twitter split button
-    jQuery('.home-quick-link a[href*="contact-us"]').on('click', false).addClass('quick-link-social hidden');
-    jQuery('.home-quick-link a[href*="contact-us"]').parent().append('<a href="https://www.facebook.com/CircleWellness" class="quick-link-social facebook"></a><a href="https://twitter.com/well4you" class="quick-link-social twitter"></a>').addClass('quick-link-social-container');
-
+    // handle mailto links
     jQuery.expr[':'].external = function(obj){
         return !obj.href.match(/^mailto\:/) && (obj.hostname !== location.hostname) && !obj.href.match(/^javascript\:/) && !obj.href.match(/^$/);
     };
