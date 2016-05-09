1416056a172fb18ff51fedd754a2ce98e3be823f
Andrew Minion <andrew@andrewrminion.com>
2016-05-09 16:52:52 -0400

    wrap functions.js in anonymous function wrapper

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,9 @@
-jQuery(document).ready(function(){
-    // handle mailto links
-    jQuery.expr[':'].external = function(obj){
-        return !obj.href.match(/^mailto\:/) && (obj.hostname !== location.hostname) && !obj.href.match(/^javascript\:/) && !obj.href.match(/^$/);
-    };
-    jQuery('a:external').attr('target', '_blank');
-});
+(function($){
+    $(document).ready(function(){
+        // handle mailto links
+        $.expr[':'].external = function(obj){
+            return !obj.href.match(/^mailto\:/) && (obj.hostname !== location.hostname) && !obj.href.match(/^javascript\:/) && !obj.href.match(/^$/);
+        };
+        $('a:external').attr('target', '_blank');
+    });
+})(jQuery);
